7e23ff99df36e7761b3fcbf101593bd537cfec3c
Conrad Weidenkeller <conrad@weidenkeller.com>
2016-03-22 03:54:05 -0500

    too many deps in setup req

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "thorium",
-    version = "0.1",
-    packages = find_packages(),
-    scripts = [],
-    install_requires = ['Flask==0.10.1',
-                        'itsdangerous==0.24',
-                        'Jinja2==2.8',
-                        'MarkupSafe==0.23',
-                        'Werkzeug==0.11.4',],
-    author = "Thorium",
-    author_email = "info@thorium.beer",
-    url = "http://thorium.beer"
+    name='thorium',
+    version='0.1',
+    packages=find_packages(),
+    scripts=[],
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+                      'flake8==2.5.4',
+                      'Flask==0.10.1',
+                      'pylint==1.5.5',
+                      'tox==2.3.1',]
+    author="Thorium",
+    author_email="info@thorium.beer",
+    url="http://thorium.beer"
 )
