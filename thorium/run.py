8454e41baa9026e5d7453b3c490249f013ddbccb
Conrad Weidenkeller <conrad@weidenkeller.com>
2016-03-28 04:14:40 -0500

    actually add favicon

--- thorium/run.py
+++ thorium/run.py
@@ -2,16 +2,11 @@ from flask import Flask, Blueprint, redirect
 from thorium.site import site
 
 app = Flask(__name__)
-FAVICON_URL=('http://dd58d482324888e2ee02-a7b127e0d8b7487c5933cbe965f92cbd.'
-            'r61.cf1.rackcdn.com/favicon.ico')
 
 @app.errorhandler(404)
 def page_not_found(error):
     return "PAGE NOT FOUND", 404
 
-@app.route('/favicon.ico', methods=['GET'])
-def favicon():
-    return redirect(FAVICON_URL)
 
 def run():
     app.register_blueprint(site)
