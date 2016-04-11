0fa82d5f9c5c58d35980fcc5ce1ad3c34350e817
Conrad Weidenkeller <conrad@weidenkeller.com>
2016-04-11 04:05:43 -0500

    Update some DB stuff

--- thorium/db.py
+++ thorium/db.py
@@ -5,23 +5,19 @@ class Beer(db.Model):
     url_name = db.Column(db.String(32), unique=True)
     name = db.Column(db.String(32))
     style = db.Column(db.String(32))
-    description = db.Column(db.String(1024))
     abv = db.Column(db.Float)
-    srm = db.Column(db.Float)
     ibu = db.Column(db.Float)
-    beer_img_url = db.Column(db.String(1024))
-    logo_url = db.Column(db.String(1024))
+    srm = db.Column(db.Float)
+    description = db.Column(db.String(1024))
 
-    def __init__(self, name, style, description, abv,
-                 srm, ibu, beer_img_url, logo_url):
+    def __init__(self, url_name, name, style, abv, ibu, srm, description):
+        self.url_name = url_name
         self.name = name
         self.style = style
         self.description = description
         self.abv = abv
-        self.srm = srm
         self.ibu = ibu
-        self.beer_img_url = beer_img_url
-        self.logo_url = logo_url
+        self.srm = srm
 
     def __repr__(self):
         return '<Beer {0}'.format(self.name)
